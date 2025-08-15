341235d407f75b62ad6c7e075f42a30427603e77
seweryn <sewerynmichota@gmail.com>
2025-08-15 01:00:53 +0100

    Add enableServiceX flag

--- src/main/java/com/severinu/blankdemoapi/controller/domain/customer/CustomerController.java
+++ src/main/java/com/severinu/blankdemoapi/controller/domain/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package com.severinu.blankdemoapi.controller.domain.customer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @Value("${serviceX.enabled}")
+    private boolean serviceXEnabled;
+
     @GetMapping
     public String getPerson() {
+        if(serviceXEnabled) {
+            System.out.println("serviceXEnabled");
+        } else {
+            System.out.println("serviceXDisabled");
+        }
         return "Customer";
     }
 }
